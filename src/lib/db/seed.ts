a445ffd980879712f2e015617a33ada9ddc74e50
Your Name <mohamed.bouazzouz@gmail.com>
2025-07-09 20:39:43 +0200

    connect to mongodb

---
+++ src/lib/db/seed.ts
@@ -0,0 +1,23 @@
+import { connectToDatabase } from '.'
+import data from '../data'
+import { cwd } from 'process'
+import { loadEnvConfig } from '@next/env'
+import Product from './models/product.model'
+loadEnvConfig(cwd())
+const main = async () => {
+  try {
+    const { products } = data
+    await connectToDatabase(process.env.MONGODB_URI)
+    await Product.deleteMany()
+    const createdProducts = await Product.insertMany(products)
+    console.log({
+      createdProducts,
+      message: 'Seeded database successfully',
+    })
+    process.exit(0)
+  } catch (error) {
+    console.error(error)
+    throw new Error('Failed to seed database')
+  }
+}
+main()
