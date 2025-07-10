0b47fe6592e5840d96b093b22f66a31b936c4b67
Your Name <mohamed.bouazzouz@gmail.com>
2025-07-10 14:03:28 +0200

    create product details page

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,3 +4,4 @@ export const APP_DESCRIPTION =
   'An Amazon clone built with Next.js, MongoDB, Shadcn'
 export const APP_SLOGAN =
   process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy.'
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
